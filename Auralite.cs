ec2fb7e01e84d320d052c70b18003cc96417704f
Graydee <wiseguy0002@gmail.com>
2016-07-31 19:30:14 -0400

    Added music

--- Auralite.cs
+++ Auralite.cs
@@ -1,4 +1,9 @@
+using System;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Auralite.Tiles;
+
 
 namespace Auralite
 {
@@ -13,6 +18,24 @@ namespace Auralite
 				AutoloadSounds = true,
                 AutoloadBackgrounds = true
             };
+		}
+		public override void UpdateMusic(ref int music)
+		{
+			Player player = Main.player[Main.myPlayer];
+
+			//Don't override the songs in this list!
+			int[] NoOverride = {MusicID.Boss1, MusicID.Boss2, MusicID.Boss3, MusicID.Boss4, MusicID.Boss5,
+				MusicID.LunarBoss, MusicID.PumpkinMoon, MusicID.TheTowers, MusicID.FrostMoon, MusicID.GoblinInvasion,
+				MusicID.PirateInvasion};
+
+			bool playMusic = true;
+			foreach(int n in NoOverride) {
+				if(music == n) playMusic = false;
+			}
+
+			if(player.active && player.GetModPlayer<AuralitePlayer>(this).ZoneSlime && !Main.gameMenu && playMusic) {
+					music = this.GetSoundSlot(SoundType.Music, "Sounds/Music/SlimeDen");
+			}
 		}
 	}
 }
