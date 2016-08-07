14cf3fe74739e0cc3c0b3eb3e2a997cd5cf1556c
EchoNex <hasohasacz@gmail.com>
2016-08-07 16:18:39 +0200

    Added Mystic staff

--- Items/Weapons/MysticStaff.cs
+++ Items/Weapons/MysticStaff.cs
@@ -14,13 +14,13 @@ namespace Auralite.Items.Weapons
         float DistX = 0f;
         public override void SetDefaults()
         {
-            item.name = "Nebula staff";
+            item.name = "Mystic Staff";
             item.damage = 20; //The damage
             item.magic = true; //Whether or not it is a magic weapon
             item.width = 56; //Item width
             item.height = 56; //Item height
             item.maxStack = 1; //How many of this item you can stack
-            item.toolTip = "Launches a bolt"; //The item’s tooltip
+            item.toolTip = "Launches a mana star"; //The item’s tooltip
             item.useTime = 6; //How long it takes for the item to be used
             item.useAnimation = 6; //How long the animation of the item takes
             Item.staff[item.type] = true;
@@ -29,7 +29,7 @@ namespace Auralite.Items.Weapons
             item.useStyle = 5; //How the weapon is held, 5 is the gun hold style
             item.value = 120000; //How much the item is worth
             item.rare = 8; //The rarity of the item
-            item.shoot = mod.ProjectileType("MysticBolt");
+            item.shoot = mod.ProjectileType("ManaStarProjectile");
             item.shootSpeed = 6f; //How fast the projectile fires 
             item.channel = true;  
             item.mana = 5;
