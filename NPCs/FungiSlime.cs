576e484bf14293dbb19e27231fc5df41eb6d7635
EchoNex <hasohasacz@gmail.com>
2016-08-12 18:39:31 +0200

    fixed fungislime

--- NPCs/FungiSlime.cs
+++ NPCs/FungiSlime.cs
@@ -32,11 +32,10 @@ namespace Auralite.NPCs
 
 		public override void NPCLoot()
 		{
-			if (Main.rand.Next(2) == 0)
-			{
+
 				Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("FungiGel"), Main.rand.Next(1, 5));
-			}
-		} */
+			
+		}
 		
 		  public override void AI()
 		  {
