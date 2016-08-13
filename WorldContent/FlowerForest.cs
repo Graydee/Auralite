b1e8982b1023151a81f882b46220957622cb3478
Graydee <wiseguy0002@gmail.com>
2016-08-13 13:58:46 -0400

    Added stuffs

--- WorldContent/FlowerForest.cs
+++ WorldContent/FlowerForest.cs
@@ -6,39 +6,158 @@ using Terraria.ModLoader;
 using Terraria.World.Generation;
 using Microsoft.Xna.Framework;
 using Terraria.GameContent.Generation;
+using System;
 
 namespace Auralite.WorldContent
 {
 	public class FlowerForest : ModWorld
 	{
+		private int WillGenn = 0;
 		public static int FlowerTiles = 0;
         public int Num;
+		private int Meme;
 
-		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
-		{
-			int ShiniesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Shinies"));
-			if (ShiniesIndex == -1)
-			{
-				return;
-			}
-			tasks.Insert(ShiniesIndex + 1, new PassLegacy("SpoilerGen", delegate(GenerationProgress progress)
-			{
-                progress.Message = "Growing spoilers";
+		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)           
+        {
+            Random rand = new Random();
+			int XTILE = WorldGen.genRand.Next(50, Main.maxTilesX - 300);
+            int xAxis = XTILE;
+			int xAxisMid = xAxis + 40;
+			int xAxisEdge = xAxis + 160;
+            int yAxis = 0;
+
+            int genIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Final Cleanup"));
+            tasks.Insert(genIndex + 1, new PassLegacy("Growing flower forest", delegate (GenerationProgress progress)
                
-                for (int i = 0; i < (int)Main.maxTilesX / 250; i++)
-				{
-					int Xvalue = WorldGen.genRand.Next(50, Main.maxTilesX - 700);
-					int Yvalue = (int)Main.worldSurface;
-					int XvalueHigh = Xvalue + 40;
-					int YvalueHigh = Yvalue + 40;
-					int XvalueMid = Xvalue + 20;
-					int YvalueMid = Yvalue + 20;
-					
-					WorldGen.TileRunner(XvalueMid, YvalueMid + 20, (double)WorldGen.genRand.Next(20,20), 1, mod.TileType("[spoiler tile]"), false, 0f, 0f, true, true); 
+            {
 
-				}
-			}));
-	}
+                for (int y = 0; y < Main.maxTilesY / 2; y++)
+                {
+                    yAxis++;
+                    xAxis = XTILE;
+
+                for (int i = 0; i < 200; i++)
+                {
+                    xAxis++;
+
+
+                        if (Main.tile[xAxis, yAxis] != null)
+                        {
+                            if (Main.tile[xAxis, yAxis].active())
+                            {
+                                int[] TileArray = { 0, 53, 112, 234, 116, 59, 147, 40};
+                                for (int BiomeDirtReplace = 0; BiomeDirtReplace < 7; BiomeDirtReplace++)
+                                {
+                                    if (Main.tile[xAxis, yAxis].type == (ushort)TileArray[BiomeDirtReplace])
+                                    {
+
+                                        if (Main.tile[xAxis, yAxis + 1] == null)
+                                        {
+                                            if (rand.Next(0, 50) == 1)
+                                            {
+												WillGenn = 0;
+											if (xAxis < xAxisMid - 1)
+											{
+												Meme = xAxisMid - xAxis;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (xAxis > xAxisEdge + 1)
+											{
+												Meme = xAxis - xAxisEdge;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (WillGenn < 10)
+											{
+                                                Main.tile[xAxis, yAxis].type = (ushort)mod.TileType("FlowerGrass");
+											}
+                                            }
+                                        }
+                                        else
+                                        {
+											WillGenn = 0;
+											if (xAxis < xAxisMid - 1)
+											{
+												Meme = xAxisMid - xAxis;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (xAxis > xAxisEdge + 1)
+											{
+												Meme = xAxis - xAxisEdge;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (WillGenn < 10)
+											{
+                                            Main.tile[xAxis, yAxis].type = (ushort)mod.TileType("FlowerGrass");
+											}
+                                        }
+
+                                        
+                                    }
+                                }
+
+                                int[] TileArray1 = { 2, 23, 60, 199 };
+                                for (int BiomeGrassReplace = 0; BiomeGrassReplace < 3; BiomeGrassReplace++)
+                                {
+                                    if (Main.tile[xAxis, yAxis].type == (ushort)TileArray1[BiomeGrassReplace])
+                                    {
+                                        if (Main.tile[xAxis, yAxis + 1] == null)
+                                        {
+                                            if (rand.Next(0, 50) == 1)
+                                            {
+												WillGenn = 0;
+											if (xAxis < xAxisMid - 1)
+											{
+												
+												Meme = xAxisMid - xAxis;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (xAxis > xAxisEdge + 1)
+											{
+												Meme = xAxis - xAxisEdge;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (WillGenn < 12)
+											{
+                                                Main.tile[xAxis, yAxis].type = (ushort)mod.TileType("FlowerGrass");
+											}
+                                            }
+                                        }
+                                        else
+                                        {
+											WillGenn = 0;
+											if (xAxis < xAxisMid - 1)
+											{
+												Meme = xAxisMid - xAxis;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (xAxis > xAxisEdge + 1)
+											{
+												Meme = xAxis - xAxisEdge;
+												WillGenn = Main.rand.Next(Meme);
+											}
+											if (WillGenn < 12)
+											{
+                                            Main.tile[xAxis, yAxis].type = (ushort)mod.TileType("FlowerGrass");
+											}
+                                        }
+                                    }
+
+                                }
+
+                                if (Main.tile[xAxis, yAxis].type == (ushort)mod.TileType("WaterGrassTile"));
+                                        
+                            }
+
+                        }
+
+
+                    }
+
+                }
+
+            }));
+
+        }
         public override void PostWorldGen()
         {
         }
